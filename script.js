16c73331b51750788c7eb51cd2c514be07b11020
bwire11 <andrewbwire68@gmail.com>
2026-05-19 18:39:07 +0300

    connected cms to frontend

--- script.js
+++ script.js
@@ -1,41 +1,28 @@
 const blogContainer = document.getElementById("blogContainer");
 
-const posts = [
-  {
-    title: "Nike Air Max 2026",
-    image: "https://images.unsplash.com/photo-1542291026-7eec264c27ff",
-    description: "Modern comfort meets street fashion."
-  },
+fetch("./posts/posts.json")
+  .then(response => response.json())
+  .then(data => {
 
-  {
-    title: "Adidas Street Runner",
-    image: "https://images.unsplash.com/photo-1600185365483-26d7a4cc7519",
-    description: "Perfect sneakers for everyday movement."
-  },
+    data.posts.reverse().forEach(post => {
 
-  {
-    title: "Puma Elite Sport",
-    image: "https://images.unsplash.com/photo-1608231387042-66d1773070a5",
-    description: "A bold design for energetic lifestyles."
-  }
-];
+      const card = document.createElement("div");
 
-posts.forEach(post => {
+      card.classList.add("blog-card");
 
-  const card = document.createElement("div");
-  card.classList.add("blog-card");
+      card.innerHTML = `
+      
+        <img src="${post.image}" alt="">
+        
+        <div class="blog-content">
+          <h2>${post.title}</h2>
+          <p>${post.description}</p>
+        </div>
+      
+      `;
 
-  card.innerHTML = `
-  
-    <img src="${post.image}" alt="">
-    
-    <div class="blog-content">
-      <h2>${post.title}</h2>
-      <p>${post.description}</p>
-    </div>
-  
-  `;
+      blogContainer.appendChild(card);
 
-  blogContainer.appendChild(card);
+    });
 
-});
+  });
